b8c94b0bd94102ad7bc9cdc06bd4b6ea28d731e6
Bahrul24 <dimasbahrulikhwani851@gmail.com>
2024-12-13 14:44:39 +0700

    Keduaaa

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+    @section()
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Profile') }}
@@ -27,3 +28,6 @@
         </div>
     </div>
 </x-app-layout>
+
+    @endsection
+    
