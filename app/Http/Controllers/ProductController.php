e76eb0c2d1cc6eca4d4bc44e5d65da74c929ded1
Bahrul24 <dimasbahrulikhwani851@gmail.com>
2024-12-13 15:09:39 +0700

    Pertama

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -48,7 +48,7 @@ class ProductController extends Controller
 
     if (!$product) {
         return response()->json(['message' => 'Product not found'], 404);
-    }
+    }   
 
     $product->update($request->all());
     
